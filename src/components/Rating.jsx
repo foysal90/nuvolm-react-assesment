033b19f99f67cefdf4f0ce00b79cb96d8c35db1d
kayato <h.kato345@gmail.com>
2022-01-19 15:17:36 +0900

    Update rating

---
+++ src/components/Rating.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rating = ({ rate }) => {
+  return (
+    <div className="card">
+      <img className="card__image" src="https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg" />
+      <div className="card__title">Mens Casual Slim Fit</div>
+    </div>
+  );
+};
+
+export default Rating;
